8b0f93f42ea37f4f1db29313439cc133d1639c65
Mitch <36958159+MitchMitch42@users.noreply.github.com>
2026-05-19 11:22:48 +0200

    enabled all my standard watch faces in movement_config.h

--- movement_config.h
+++ movement_config.h
@@ -29,16 +29,19 @@
 
 const watch_face_t watch_faces[] = {
     clock_face,
-    world_clock_face,
-    sunrise_sunset_face,
-    moon_phase_face,
+    timer_face,
+    advanced_alarm_face,
     fast_stopwatch_face,
-    countdown_face,
-    alarm_face,
+
+    moon_phase_face,
+    sunrise_sunset_face,
+    world_clock_face,
     temperature_display_face,
     voltage_face,
     settings_face,
     set_time_face,
+    finetune_face,
+    nanosec_face
 };
 
 #define MOVEMENT_NUM_FACES (sizeof(watch_faces) / sizeof(watch_face_t))
@@ -49,7 +52,7 @@ const watch_face_t watch_faces[] = {
  * Some folks also like to use this to hide the preferences and time set faces from the normal rotation.
  * If you don't want any faces to be excluded, set this to 0 and a long Mode press will have no effect.
  */
-#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 5)
+#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 9)
 
 /* Custom hourly chime tune. Check movement_custom_signal_tunes.h for options. */
 #define SIGNAL_TUNE_DEFAULT
