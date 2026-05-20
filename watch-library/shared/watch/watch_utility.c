b6d72ab10dc0f3bf4339281916a5647314b4d1e1
Mitch <36958159+MitchMitch42@users.noreply.github.com>
2026-05-20 17:17:08 +0200

    German weekdays

--- watch-library/shared/watch/watch_utility.c
+++ watch-library/shared/watch/watch_utility.c
@@ -28,7 +28,7 @@
 #include "zones.h"
 
 const char * watch_utility_get_weekday(watch_date_time_t date_time) {
-    static const char weekdays[7][3] = {"MO", "TU", "WE", "TH", "FR", "SA", "SU"};
+    static const char weekdays[7][3] = {"MO", "DI", "MI", "DO", "FR", "SA", "SO"};
     return weekdays[watch_utility_get_iso8601_weekday_number(date_time.unit.year + WATCH_RTC_REFERENCE_YEAR, date_time.unit.month, date_time.unit.day) - 1];
 }
 
