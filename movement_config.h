8521054e3274e6e19e77e141683196d0f7e787f5
Mitch <36958159+MitchMitch42@users.noreply.github.com>
2026-05-27 19:38:39 +0200

    do not use tamagotchi

--- movement_config.h
+++ movement_config.h
@@ -35,13 +35,12 @@ const watch_face_t watch_faces[] = {
 
     moon_phase_face,
     sunrise_sunset_face,
-    world_clock_face,
     temperature_display_face,
     temperature_correction_face,
     voltage_face,
     endless_runner_face,
 
-    tamagotchi_face,
+    world_clock_face,
     settings_face,
     blink_receiver_face,
     set_time_face,
@@ -58,7 +57,7 @@ const watch_face_t watch_faces[] = {
  * Some folks also like to use this to hide the preferences and time set faces from the normal rotation.
  * If you don't want any faces to be excluded, set this to 0 and a long Mode press will have no effect.
  */
-#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 14)
+#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 13)
 #define MOVEMENT_TERTIARY_FACE_INDEX (MOVEMENT_NUM_FACES - 7)
 
 /* Custom hourly chime tune. Check movement_custom_signal_tunes.h for options. */
