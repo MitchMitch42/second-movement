ab86d9b1331727b64731c1344924d25bb94f6415
Mitch <36958159+MitchMitch42@users.noreply.github.com>
2026-05-27 12:06:30 +0200

    Blink receiver (#8)
    * Added blink_receiver_face
    * Added frequency_rising_edge, added logging
    * Added exact timestamp, added basic handling of multiple packets

--- movement_faces.h
+++ movement_faces.h
@@ -83,6 +83,7 @@
 #include "tomato_face.h"
 #include "solar_time_face.h"
 #include "tide_face.h"
+#include "blink_receiver_face.h"
 #include "tamagotchi_face.h"
 #include "temperature_correction_face.h"
 // New includes go above this line.
