2e8ddc70326fe41713c147b044fff9c1006df23a
Mitch <36958159+MitchMitch42@users.noreply.github.com>
2026-05-27 19:39:09 +0200

    Squashed commit of the following:
    commit 8521054e3274e6e19e77e141683196d0f7e787f5
    Author: Mitch <36958159+MitchMitch42@users.noreply.github.com>
    Date:   Wed May 27 19:38:39 2026 +0200
    
        do not use tamagotchi
    
    commit 425d16584ef2095705720269ed507b287c57a5d4
    Author: Mitch <36958159+MitchMitch42@users.noreply.github.com>
    Date:   Wed May 27 14:43:27 2026 +0200
    
        added endless_runner
    
    commit 03be6346d2be709d81efd1e0a62ae0d57965fa49
    Author: Mitch <36958159+MitchMitch42@users.noreply.github.com>
    Date:   Wed May 27 14:40:17 2026 +0200
    
        different wording in sunrise_sunset_face
    
    commit 2c3b9601153318831f6ae1d6d8db151ac9539ef7
    Author: Mitch <36958159+MitchMitch42@users.noreply.github.com>
    Date:   Wed May 27 14:26:50 2026 +0200
    
        adapted config
    
    commit 685e491c13028579429067ba674c49862a951ba4
    Merge: d851a4e b07df1e
    Author: Mitch <36958159+MitchMitch42@users.noreply.github.com>
    Date:   Wed May 27 14:24:19 2026 +0200
    
        Merge branch 'main' into dev
    
    commit d851a4ef96b18a4eab09459a7fe41bb55e703928
    Author: Mitch <36958159+MitchMitch42@users.noreply.github.com>
    Date:   Wed May 27 13:36:43 2026 +0200
    
        adapted CLOCK_FACE_LOW_BATTERY_VOLTAGE_THRESHOLD
    
    commit 5ba38acd2656bd439c549577762e72348a50a0e5
    Author: Mitch <36958159+MitchMitch42@users.noreply.github.com>
    Date:   Wed May 27 13:14:11 2026 +0200
    
        added easteregg_face in config
    
    commit ef800a90d3375d94a9d0f00df3ced1e9a88dd202
    Merge: 377dedb 8c9f313
    Author: Mitch <36958159+MitchMitch42@users.noreply.github.com>
    Date:   Wed May 27 13:13:27 2026 +0200
    
        Merge branch 'main' into dev
    
    commit 377dedbfb2468b104c67b914078c654597b690d6
    Author: Mitch <36958159+MitchMitch42@users.noreply.github.com>
    Date:   Wed May 27 12:45:29 2026 +0200
    
        added faces in config
    
    commit 44ddc0669b6d6be61cd8f97f3ae171520459b220
    Author: Mitch <36958159+MitchMitch42@users.noreply.github.com>
    Date:   Wed May 27 12:36:57 2026 +0200
    
        My config

--- movement_config.h
+++ movement_config.h
@@ -29,16 +29,24 @@
 
 const watch_face_t watch_faces[] = {
     clock_face,
-    world_clock_face,
-    sunrise_sunset_face,
-    moon_phase_face,
+    timer_face,
+    advanced_alarm_face,
     fast_stopwatch_face,
-    countdown_face,
-    alarm_face,
+
+    moon_phase_face,
+    sunrise_sunset_face,
     temperature_display_face,
+    temperature_correction_face,
     voltage_face,
+    endless_runner_face,
+
+    world_clock_face,
     settings_face,
+    blink_receiver_face,
     set_time_face,
+    easteregg_face,
+    finetune_face,
+    nanosec_face
 };
 
 #define MOVEMENT_NUM_FACES (sizeof(watch_faces) / sizeof(watch_face_t))
@@ -49,8 +57,8 @@ const watch_face_t watch_faces[] = {
  * Some folks also like to use this to hide the preferences and time set faces from the normal rotation.
  * If you don't want any faces to be excluded, set this to 0 and a long Mode press will have no effect.
  */
-#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 5)
-#define MOVEMENT_TERTIARY_FACE_INDEX (MOVEMENT_NUM_FACES - 2)
+#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 13)
+#define MOVEMENT_TERTIARY_FACE_INDEX (MOVEMENT_NUM_FACES - 7)
 
 /* Custom hourly chime tune. Check movement_custom_signal_tunes.h for options. */
 #define SIGNAL_TUNE_DEFAULT
