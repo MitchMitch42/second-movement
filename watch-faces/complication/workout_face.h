0c5cb1d34cf357224627fc83d4ef5b57f0f3c191
Mitch <36958159+MitchMitch42@users.noreply.github.com>
2026-09-18 19:33:52 +0200

    copied fast_stopwatch

--- watch-faces/complication/workout_face.h
+++ watch-faces/complication/workout_face.h
@@ -1,41 +1,30 @@
 /*
  * MIT License
  *
- * Copyright (c) <#year#> <#author_name#>
+ * Copyright (c) 2026
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
  */
 
-#pragma once
+#ifndef WORKOUT_FACE_H_
+#define WORKOUT_FACE_H_
+
+/*
+ * WORKOUT STOPWATCH face (copy of fast_stopwatch_face renamed)
+ */
 
 #include "movement.h"
 
-/*
- * A DESCRIPTION OF YOUR WATCH FACE
- *
- * and a description of how use it
- *
- */
-
 typedef struct {
-    // Anything you need to keep track of, put it here!
-    uint8_t unused;
+    rtc_counter_t start_counter; // rtc counter when the stopwatch was started
+    rtc_counter_t lap_counter;   // rtc counter when the stopwatch was lapped
+    rtc_counter_t stop_counter;  // rtc counter when the stopwatch was stopped
+    uint8_t status;              // the status the stopwatch is in (idle, running, stopped)
+    bool slow_refresh;           // update the display slowly (same 128Hz timekeeping accuracy)
+    struct {
+        rtc_counter_t seconds;
+        rtc_counter_t minutes;
+        rtc_counter_t hours;
+    } old_display;               // the digits currently being displayed on screen
 } workout_state_t;
 
 void workout_face_setup(uint8_t watch_face_index, void ** context_ptr);
@@ -50,3 +39,5 @@ void workout_face_resign(void *context);
     workout_face_resign, \
     NULL, \
 })
+
+#endif // WORKOUT_FACE_H_
